e599a1349231b5f9177813aa1ce05bfb42c61d68
jiAlbert_Lee_company_eclipse <3185017028@qq.com>
2019-02-14 19:46:54 +0800

    添加手绘画Activity和View

---
+++ app/src/main/java/textimagemaker/hooyee/com/textimagemaker/main/TActivity.java
@@ -0,0 +1,18 @@
+package textimagemaker.hooyee.com.textimagemaker.main;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import textimagemaker.hooyee.com.textimagemaker.R;
+
+/**
+ * 手画
+ */
+public class TActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t);
+    }
+}
