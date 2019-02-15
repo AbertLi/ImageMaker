74bbecb02171428d535f0b5baf9e994743c6dbe9
jiAlbert_Lee_company_eclipse <3185017028@qq.com>
2019-02-15 11:08:26 +0800

    1，实现手绘图效果，接下来把手绘图转换成图片保存起来。 2，接下来：单击的时候也能画上，不只是move才能绘制。 3，接下来：添加撤回上一步操作。

--- app/src/main/java/textimagemaker/hooyee/com/textimagemaker/main/ShhActivity.java
+++ app/src/main/java/textimagemaker/hooyee/com/textimagemaker/main/ShhActivity.java
@@ -14,5 +14,6 @@ public class ShhActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t);
+
     }
 }
